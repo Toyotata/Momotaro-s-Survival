ff83ee2553ac95c4f3df610fd40ea17cdc26271a
Toyotata <86489791+Toyotata@users.noreply.github.com>
2023-06-03 17:18:30 +0900

    toyoda

--- Assets/Project/Program/GameScenes/Script/Stage/StageManager.cs
+++ Assets/Project/Program/GameScenes/Script/Stage/StageManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//タイルマップを操作する時に必要
+using UnityEngine.Tilemaps;
+//シーンの読み込み/再読み込み時に必要　SceneManager.LoadScene("SampleScene");
+using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Tilemap tilemap;
+    [SerializeField] Tile grassTile;
+
     void Start()
     {
-        
+        StartCoroutine(SetTile());
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator SetTile()
     {
-        
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                tilemap.SetTile(new Vector3Int(x, y, 0), grassTile);
+                yield return new WaitForEndOfFrame();
+            }
+        }
     }
 }
