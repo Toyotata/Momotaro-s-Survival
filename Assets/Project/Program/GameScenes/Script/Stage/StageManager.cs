e326699d3c0f28411d806803fe80fbd92f0a29a6
Toyotata <86489791+Toyotata@users.noreply.github.com>
2023-06-13 21:15:01 +0900

    toyoda

--- Assets/Project/Program/GameScenes/Script/Stage/StageManager.cs
+++ Assets/Project/Program/GameScenes/Script/Stage/StageManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 //タイルマップを操作する時に必要
 using UnityEngine.Tilemaps;
-//シーンの読み込み/再読み込み時に必要　SceneManager.LoadScene("SampleScene");
+//シーンの読み込み/再読み込み時に必要
 using UnityEngine.SceneManagement;
 
 public class StageManager : MonoBehaviour
@@ -11,15 +11,38 @@ public class StageManager : MonoBehaviour
     [SerializeField] Tilemap tilemap;
     [SerializeField] Tile grassTile;
 
+    //マップの最大数
+    int i_max_x;
+    int i_max_y;
+
+    //マップの最小数
+    int i_min_x;
+    int i_min_y;
+
     void Start()
     {
+        //マップの最大値設定
+        i_max_x = 0;
+        i_max_y = 0;
+
+        //マップの最小値設定
+        int i_min_x = 0;
+        int i_min_y = 0;
+
         StartCoroutine(SetTile());
     }
-    IEnumerator SetTile()
+
+    /// <summary>
+    /// スタート時マップ作製処理
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator SetTile()
     {
-        for (int y = 0; y < 5; y++)
+        ////マップをクリアする（重複しないようにする）
+        tilemap.ClearAllTiles();
+        for (int y = i_min_y-22; y < i_max_y+23; y++)
         {
-            for (int x = 0; x < 5; x++)
+            for (int x = i_min_x-50; x < i_max_x+28; x++)
             {
                 tilemap.SetTile(new Vector3Int(x, y, 0), grassTile);
                 yield return new WaitForEndOfFrame();
